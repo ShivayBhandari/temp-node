22d74ef663c4af5b77bc7682c70c415c574cd7d9
Shivay <bhandarishivay2001@gmail.com>
2022-02-12 23:08:53 +0530

    first commit

---
+++ 10-fs-sync.js
@@ -0,0 +1,11 @@
+const { readFileSync, writeFileSync } = require('fs'); //or
+const { syncBuiltinESMExports } = require('module');
+// const fs = require('fs');
+// fs.readFileSync;
+
+const first = readFileSync('./content/first.txt', 'utf8');
+const second = readFileSync('./content/second.txt', 'utf8');
+
+console.log(first, second);
+
+writeFileSync('./content/result-sync.txt', `Here is the result: ${first}, ${second}`, { flag: 'a' });
---
+++ 3-modules.js
@@ -0,0 +1,7 @@
+// Modules
+const names = require('./4-names')
+const sayHi = require('./5-function')
+require('./7-mind-grenade')
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
